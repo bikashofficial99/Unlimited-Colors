aff409a7eca6294e7c3d69c53891a374652bebbd
Bikash Acharya <bikash.acharya614@gmail.com>
2024-10-06 09:55:04 +0545

    js logic updated

--- uc.js
+++ uc.js
@@ -1,22 +1,25 @@
 const randomcolor = function () {
-    const hex = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += hex[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
-  let intervalId;
-  const startChangingColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalId;
+const startChangingColor = function () {
+  if (!intervalId) {
     intervalId = setInterval(changeBgcolor, 1000);
+  }
 
-    function changeBgcolor() {
-      document.body.style.backgroundColor = randomcolor();
-    }
-  };
-  const stopChangingColor = function () {
-    clearInterval(intervalId);
-  };
+  function changeBgcolor() {
+    document.body.style.backgroundColor = randomcolor();
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
 
-  document.querySelector("#start").addEventListener("click", startChangingColor);
-  document.querySelector("#stop").addEventListener("click", stopChangingColor);
+document.querySelector("#start").addEventListener("click", startChangingColor);
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
